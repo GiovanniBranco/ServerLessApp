1c686a2cc01c6eeae478e0eb0260f9c78fc708c5
Giovanni Branco <giovanni.branco@dtidigital.com.br>
2024-04-18 19:34:36 -0300

    feat: iniciando configuracao da api serverless com as funcoes da api em express

---
+++ api/index.js
@@ -0,0 +1,73 @@
+const { randomUUID } = require("crypto");
+
+const extractBody = (event) => {
+  if (!event?.body)
+    return {
+      statusCode: 422,
+      body: JSON.stringify({
+        error: "Missing body",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+  return JSON.parse(event.body);
+};
+
+module.exports.sendResponse = async (event) => {
+  const body = extractBody(event);
+  const { name, answers } = body;
+
+  const correctQuestions = [3, 1, 0, 2];
+
+  const correctAnswers = answers.reduce((acc, answer, index) => {
+    if (answer === correctQuestions[index]) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+
+  const result = {
+    name,
+    correctAnswers,
+    totalAnswers: answers.length,
+  };
+
+  const resultId = randomUUID();
+  previousResults.set(resultId, { response: body, result });
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      resultId,
+      __hypermedia: {
+        href: `/results.html`,
+        query: { id: resultId },
+      },
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
+
+module.exports.getResult = async (event) => {
+  const result = previousResults.get(event.pathParameters.id);
+  if (!result)
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Result not found" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
